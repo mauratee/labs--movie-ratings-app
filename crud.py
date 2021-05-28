4d4ddde184bc1b0a10d3639a685282d4a7d679ca
Maura Thompson <thomaura@gmail.com>
2021-05-28 22:24:07 +0000

    Initial commit

---
+++ crud.py
@@ -0,0 +1,50 @@
+"""CRUD operations."""
+import datetime
+from model import db, User, Movie, Rating, connect_to_db
+
+
+def create_user(email, password):
+    """Create and return a new user."""
+
+    user = User(email=email, password=password)
+
+    db.session.add(user)
+    db.session.commit()
+
+    return user
+
+
+def create_movie(title, overview, release_date, poster_path):
+    """Create and return a new movie."""
+
+    movie = Movie(title=title, overview=overview, 
+                  release_date=release_date, poster_path=poster_path)
+    
+    db.session.add(movie)
+    db.session.commit()
+
+    return movie
+
+
+def view_all_movies(movies):
+    """Allow user to view all movies in movies table"""
+
+    return Movie.query.all()
+
+
+
+def create_rating(user, movie, score):
+    """Create and return a rating for a movie by a user"""
+
+    rating = Rating(user=user, movie=movie, score=score)
+
+    db.session.add(rating)
+    db.session.commit()
+
+    return rating
+
+
+
+if __name__ == '__main__':
+    from server import app
+    connect_to_db(app)
